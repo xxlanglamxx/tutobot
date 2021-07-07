d25f0feba0c02f73bc84e398afa07b2966dbdb97
xxlanglamxx <87083638+xxlanglamxx@users.noreply.github.com>
2021-07-07 17:48:56 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,82 @@
+const Discord = require('discord.js'),
+    client = new Discord.Client({
+        fetchAllMembers: true
+    }),
+    config = require('./config.json'),
+    fs = require('fs')
+
+client.login(config.token)
+client.commands = new Discord.Collection()
+client.db = require('./db.json')
+
+fs.readdir('./commands', (err, files) => {
+    if (err) throw err
+    files.forEach(file => {
+        if (!file.endsWith('.js')) return
+        const command = require(`./commands/${file}`)
+        client.commands.set(command.name, command)
+    })
+})
+ 
+client.on('message', message => {
+    if (message.type !== 'DEFAULT' || message.author.bot) return
+ 
+    const args = message.content.trim().split(/ +/g)
+    const commandName = args.shift().toLowerCase()
+    if (!commandName.startsWith(config.prefix)) return
+    const command = client.commands.get(commandName.slice(config.prefix.length))
+    if (!command) return
+    command.run(message, args, client)
+})
+
+ 
+client.on('guildMemberAdd', member => {
+    member.guild.channels.cache.get(config.greeting.channel).send(`${member} a rejoint le serveur. Nous sommes désormais ${member.guild.memberCount} ! 🎉`)
+    member.roles.add(config.greeting.role)
+})
+ 
+client.on('guildMemberRemove', member => {
+    member.guild.channels.cache.get(config.greeting.channel).send(`${member.user.tag} a quitté le serveur... 😢`)
+})
+
+client.on('messageReactionAdd', (reaction, user) => {
+    if (!reaction.message.guild || user.bot) return
+    const reactionRoleElem = config.reactionRole[reaction.message.id]
+    if (!reactionRoleElem) return
+    const prop = reaction.emoji.id ? 'id' : 'name'
+    const emoji = reactionRoleElem.emojis.find(emoji => emoji[prop] === reaction.emoji[prop])
+    if (emoji) reaction.message.guild.member(user).roles.add(emoji.roles)
+    else reaction.users.remove(user)
+})
+ 
+client.on('messageReactionRemove', (reaction, user) => {
+    if (!reaction.message.guild || user.bot) return
+    const reactionRoleElem = config.reactionRole[reaction.message.id]
+    if (!reactionRoleElem || !reactionRoleElem.removable) return
+    const prop = reaction.emoji.id ? 'id' : 'name'
+    const emoji = reactionRoleElem.emojis.find(emoji => emoji[prop] === reaction.emoji[prop])
+    if (emoji) reaction.message.guild.member(user).roles.remove(emoji.roles)
+})
+
+client.on('ready', () => {
+    const statuses = [
+        () => `${client.guilds.cache.size} serveurs`,
+        () => `${client.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0)} utilisateurs`
+    ]
+    let i = 0
+    setInterval(() => {
+        client.user.setActivity(statuses[i](), {type: 'PLAYING'})
+        i = ++i % statuses.length
+    }, 1e4)
+})
+
+client.on('channelCreate', channel => {
+    if (!channel.guild) return
+    const muteRole = channel.guild.roles.cache.find(role => role.name === 'Muted')
+    if (!muteRole) return
+    channel.createOverwrite(muteRole, {
+        SEND_MESSAGES: false,
+        CONNECT: false,
+        ADD_REACTIONS: false
+    })
+})
